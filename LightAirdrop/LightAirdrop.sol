49e753834b6ce6e911cc639dd7f0d521a07c5bec
DESKTOP-UROPKCT\Alex <alex@asdas>
2018-03-19 19:01:24 +0100

    LightAirdrop

---
+++ LightAirdrop/LightAirdrop.sol
@@ -0,0 +1,34 @@
+pragma solidity ^0.4.21;
+
+
+import "./EIP20Interface.sol";
+import "./OwnableContract.sol";
+
+contract LightAirdrop is OwnableContract{ 
+     
+ 
+    function LightAirdrop() public { 
+    }
+     
+    function performEqual(address tokenAddress, address[] tos, uint256 amount) onlyOwner public {
+        
+        EIP20Interface tokenContract = EIP20Interface(tokenAddress);
+        
+        uint256 i;
+        uint256 n = tos.length;
+        for( ; i<n; i++) {
+            tokenContract.transfer(tos[i], amount);
+        }
+    }
+    
+    function performDifferent(address tokenAddress, address[] tos, uint256[] amounts) onlyOwner public {
+        
+        EIP20Interface tokenContract = EIP20Interface(tokenAddress);
+        
+        uint256 i;
+        uint256 n = tos.length;
+        for( ; i<n; i++) {
+            tokenContract.transfer(tos[i], amounts[i]);
+        }
+    }
+}
